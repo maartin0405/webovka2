03118f2cd9eb2c5e7ab4a438d128257619c02bbe
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-02-20 22:54:06 +0100

    hiiii :3

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "../../styles/global.css";
+import styled from "@emotion/styled";
+
+const StyledDiv = styled.div`
+  padding-bottom: 137px;
+`;
 
 const Layout = ({ children }) => {
-  return <>{children}</>;
+  return <StyledDiv>{children}</StyledDiv>;
 };
 
 export default Layout;
