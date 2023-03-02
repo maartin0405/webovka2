9aabc483bdb58c2f5907bde0e056dd8baf46fe86
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-03-02 15:23:12 +0100

    kys

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,43 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const { languages, defaultLanguage } = require("./languages");
+
+const siteUrl = `https://www.yourdomain.tld`;
+
 module.exports = {
   siteMetadata: {
     title: `webovka lololol`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl,
   },
-  plugins: ["gatsby-plugin-emotion"]
+  plugins: [
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-plugin-react-i18next",
+      options: {
+        languages,
+        defaultLanguage,
+        siteUrl,
+        i18nextOptions: {
+          // debug: true,
+          fallbackLng: defaultLanguage,
+          supportedLngs: languages,
+          defaultNS: "common",
+          interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+          },
+          react: {
+            bindI18n: "languageChanged editorSaved",
+          },
+        },
+        pages: [
+          {
+            matchPath: "/ignored-page",
+            languages: ["en"],
+          },
+        ],
+      },
+    },
+  ],
 };
