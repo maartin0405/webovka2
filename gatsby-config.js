8020594d6e6e688a4706694cabb5f60f67994068
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-03-02 23:04:49 +0100

    lll

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,6 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-const { languages, defaultLanguage } = require("./languages");
-
 const siteUrl = `https://www.yourdomain.tld`;
 
 module.exports = {
@@ -13,31 +11,5 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-emotion",
-    {
-      resolve: "gatsby-plugin-react-i18next",
-      options: {
-        languages,
-        defaultLanguage,
-        siteUrl,
-        i18nextOptions: {
-          // debug: true,
-          fallbackLng: defaultLanguage,
-          supportedLngs: languages,
-          defaultNS: "common",
-          interpolation: {
-            escapeValue: false, // not needed for react as it escapes by default
-          },
-          react: {
-            bindI18n: "languageChanged editorSaved",
-          },
-        },
-        pages: [
-          {
-            matchPath: "/ignored-page",
-            languages: ["en"],
-          },
-        ],
-      },
-    },
   ],
 };
