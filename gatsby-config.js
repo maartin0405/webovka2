d71437045d68242c9a1cc1eb56e7d75b89b04c18
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-03-03 14:07:09 +0100

    asd

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,18 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-emotion",
+    {
+      resolve: `gatsby-plugin-intl`,
+      options: {
+        // language JSON resource path
+        path: `${__dirname}/src/intl`,
+        // supported language
+        languages: [`en`, `cz`],
+        // language file path
+        defaultLanguage: `en`,
+        // option to redirect to `/ko` when connecting `/`
+        redirect: true,
+      },
+    },
   ],
 };
