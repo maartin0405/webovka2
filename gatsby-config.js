f09247f5a85dde2ae4f45c8e5dd8e60968317942
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-03-03 16:38:29 +0100

    fuck this

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,16 +22,13 @@ module.exports = {
       resolve: `gatsby-plugin-react-i18next`,
       options: {
         localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
-        languages: [`en`, `cz`],
+        languages,
         defaultLanguage: `en`,
         siteUrl,
         // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
         trailingSlash: "always",
         // you can pass any i18next options
         i18nextOptions: {
-          interpolation: {
-            escapeValue: false, // not needed for react as it escapes by default
-          },
           keySeparator: false,
           nsSeparator: false,
         },
@@ -39,7 +36,6 @@ module.exports = {
           {
             matchPath: "/:lang?/blog/:uid",
             getLanguageFromPath: true,
-            excludeLanguages: ["es"],
           },
           {
             matchPath: "/preview",
