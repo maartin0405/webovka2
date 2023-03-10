4ecfc516a2fecd8682b2f889e113dd3f30811d27
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-03-10 14:12:22 +0100

    ok

---
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Card from "../Card";
+import Logo from "../Logo";
+import RegisterForm from "../RegisterForm";
+import LanguageSwitcher from "../LanguageSwitcher";
+import Layout from "../Layout";
+
+const StyledLogin = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledForm = styled(RegisterForm)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const RegisterPage = () => {
+  return (
+    <Layout>
+      <StyledLogin>
+        <Logo width={90} />
+        <Card>
+          <LanguageSwitcher />
+          <StyledForm />
+        </Card>
+      </StyledLogin>
+    </Layout>
+  );
+};
+
+export default RegisterPage;
--- src/pages/register.en.jsx
+++ src/pages/register.en.jsx
@@ -4,32 +4,10 @@ import Card from "../components/Card";
 import Logo from "../components/Logo";
 import RegisterForm from "../components/RegisterForm";
 import LanguageSwitcher from "../components/LanguageSwitcher";
-import Layout from "../components/Layout";
-
-const StyledLogin = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const StyledForm = styled(RegisterForm)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+import RegisterPage from "../components/RegisterPage/RegisterPage";
 
 const Register = () => {
-  return (
-    <Layout>
-      <StyledLogin>
-        <Logo width={90} />
-        <Card>
-          <LanguageSwitcher />
-          <StyledForm />
-        </Card>
-      </StyledLogin>
-    </Layout>
-  );
+  return <RegisterPage />;
 };
 
 export default Register;
