bc2ea16e2b239e38cd71cc2569f62fd803d39f59
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-03-10 23:03:11 +0100

    custom Link instead of gatsby link

---
+++ src/components/Link.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link as GatsbyLink } from "gatsby-link";
+import getLangKey from "../utils/intl/getLangKey";
+import { VALID_LANG_KEYS, DEFAULT_LANG_KEY } from "../utils/intl/LANG_KEYS";
+
+const Link = ({ children, to, ...props }) => {
+  const url = typeof window !== "undefined" ? window.location.pathname : "";
+  const currentLangKey = getLangKey(url, VALID_LANG_KEYS, DEFAULT_LANG_KEY);
+  return (
+    <GatsbyLink to={`/${currentLangKey}${to}`} {...props}>
+      {children}
+    </GatsbyLink>
+  );
+};
+
+export default Link;
--- src/components/LinkAsAButton.jsx
+++ src/components/LinkAsAButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
+import Link from "./Link";
 import styled from "@emotion/styled";
 
 const StyledLink = styled(Link)`
