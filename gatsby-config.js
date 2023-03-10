c38ac0fbcbf5d9e187534af7a03b76674fb3b5e9
maartin0405 <73892568+maartin0405@users.noreply.github.com>
2023-03-10 11:38:38 +0100

    idk

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,27 @@ module.exports = {
     siteUrl,
   },
   plugins: [
-    "gatsby-plugin-emotion",   {
-      resolve: 'gatsby-plugin-i18n',
-      options: {        
-        langKeyDefault: 'en',
-        useLangKeyLayout: false
-      }
-    }
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-plugin-i18n",
+      options: {
+        langKeyDefault: "en",
+        useLangKeyLayout: false,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "<YOUR_FIREBASE_API_KEY>",
+          authDomain: "<YOUR_FIREBASE_AUTH_DOMAIN>",
+          databaseURL: "<YOUR_FIREBASE_DATABASE_URL>",
+          projectId: "<YOUR_FIREBASE_PROJECT_ID>",
+          storageBucket: "<YOUR_FIREBASE_STORAGE_BUCKET>",
+          messagingSenderId: "<YOUR_FIREBASE_MESSAGING_SENDER_ID>",
+          appId: "<YOUR_FIREBASE_APP_ID>",
+        },
+      },
+    },
   ],
 };
